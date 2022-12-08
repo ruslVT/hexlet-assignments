cd22d1d42685923b863ec47c42bb3e353c3542fe
@hexlet/cli <support@hexlet.io>
2022-12-08 14:28:42 +0300

    submit java-web-ru/rest-api

---
+++ java-web-ru/rest-api/src/main/java/exercise/controllers/UserController.java
@@ -0,0 +1,71 @@
+package exercise.controllers;
+
+import io.javalin.http.Context;
+import io.javalin.apibuilder.CrudHandler;
+import io.ebean.DB;
+import java.util.List;
+
+import exercise.domain.User;
+import exercise.domain.query.QUser;
+
+import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.commons.lang3.StringUtils;
+
+public class UserController implements CrudHandler {
+
+    public void getAll(Context ctx) {
+        // BEGIN
+        List<User> users = new QUser()
+                .orderBy()
+                    .id.asc()
+                .findList();
+
+        String json = DB.json().toJson(users);
+        ctx.json(json);
+        // END
+    };
+
+    public void getOne(Context ctx, String id) {
+
+        // BEGIN
+        User user = new QUser()
+                .id.equalTo(Integer.parseInt(id))
+                .findOne();
+
+        String json = DB.json().toJson(user);
+        ctx.json(json);
+        // END
+    };
+
+    public void create(Context ctx) {
+
+        // BEGIN
+        User user = ctx.bodyValidator(User.class)
+                        .check(obj -> !obj.getFirstName().isEmpty(), "FirstName cannot be empty")
+                        .check(obj -> !obj.getLastName().isEmpty(),"LastName cannot be empty")
+                        .check(obj -> EmailValidator.getInstance().isValid(obj.getEmail()),
+                                "Incorrect mail format")
+                        .check(obj -> StringUtils.isNumeric(obj.getPassword()),
+                                "the password must contain at least 4 digits")
+                        .get();
+        user.save();
+        // END
+    };
+
+    public void update(Context ctx, String id) {
+        // BEGIN
+        String body = ctx.body();
+        User user = DB.json().toBean(User.class, body);
+        user.setId(id);
+        user.update();
+        // END
+    };
+
+    public void delete(Context ctx, String id) {
+        // BEGIN
+        new QUser()
+                .id.equalTo(Integer.parseInt(id))
+                .delete();
+        // END
+    };
+}
